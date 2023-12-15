2d2e6aae4fc877b6feb57f06451d32f7f51d72ef
Andreas <pingis00@hotmail.com>
2023-12-15 08:44:26 +0100

    Gjort stor del av menyn

--- ContactConsoleApplication/Program.cs
+++ ContactConsoleApplication/Program.cs
@@ -1,5 +1,21 @@
 ﻿using ContactConsoleApplication.Interfaces;
 using ContactConsoleApplication.Services;
+using ContactServiceLibrary.Interfaces;
+using ContactServiceLibrary.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-IMenuService menuService = new MenuService();
+var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
+{
+    services.AddSingleton<IFileService, FileService>();
+    services.AddSingleton<IContactService, ContactService>();
+    services.AddSingleton<IContactRepository, ContactRepository>();
+    services.AddSingleton<IMenuService, MenuService>();
 
+}).Build();
+
+builder.Start();
+Console.Clear();
+
+var menuService = builder.Services.GetRequiredService<MenuService>();
+menuService.ShowMainMenu();
