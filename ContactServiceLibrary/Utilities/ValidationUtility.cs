0722a0c6f986e5665d02ed2107576088e19b6f69
Andreas <pingis00@hotmail.com>
2023-12-20 10:42:20 +0100

    Lagt till reg ex med summary-kommentarer

--- ContactServiceLibrary/Utilities/ValidationUtility.cs
+++ ContactServiceLibrary/Utilities/ValidationUtility.cs
@@ -4,6 +4,11 @@ namespace ContactServiceLibrary.Utilities;
 
 public class ValidationUtility
 {
+    /// <summary>
+    /// Validates an email address based on standard email format.
+    /// </summary>
+    /// <param name="email">The email address to validate.</param>
+    /// <returns>true if the email address is valid; otherwise, false.</returns>
     public static bool IsValidEmail(string email)
     {
         try
@@ -17,6 +22,14 @@ public class ValidationUtility
         }
     }
 
+    /// <summary>
+    /// Validates a phone number based on a general international format.
+    /// </summary>
+    /// <param name="phoneNumber">The phone number to validate.</param>
+    /// <returns>true if the phone number is valid; otherwise, false.</returns>
+    /// <remarks>
+    /// This method checks if the phone number starts with an optional plus followed by up to 15 digits.
+    /// </remarks>
     public static bool IsValidPhoneNumber(string phoneNumber)
     {
         var regex = new Regex(@"^\+?[0-9]\d{1,14}$");
