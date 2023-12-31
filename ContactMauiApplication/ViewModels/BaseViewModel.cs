190befb08fe6b84a4a52e8cf5c69a5d5e97b384f
Andreas <pingis00@hotmail.com>
2023-12-31 13:27:41 +0100

    rensat

---
+++ ContactMauiApplication/ViewModels/BaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ContactMauiApplication.ViewModels
+{
+    public partial class BaseViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public string? message;
+    }
+}
