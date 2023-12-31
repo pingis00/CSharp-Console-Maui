ee93a50d49dfe30d3c99248eff89ab9156399348
Andreas <pingis00@hotmail.com>
2023-12-31 14:52:02 +0100

    Nästan färdig

--- ContactMauiApplication/ViewModels/BaseViewModel.cs
+++ ContactMauiApplication/ViewModels/BaseViewModel.cs
@@ -5,6 +5,21 @@ namespace ContactMauiApplication.ViewModels
     public partial class BaseViewModel : ObservableObject
     {
         [ObservableProperty]
-        public string? message;
+        private string? message;
+
+        [ObservableProperty]
+        private Color messageColor = Colors.Transparent;
+
+        [ObservableProperty]
+        private bool isMessageVisible;
+
+        protected async Task ShowTemporaryMessageAsync(string newMessage, Color newColor)
+        {
+            Message = newMessage;
+            MessageColor = newColor;
+            IsMessageVisible = true;
+            await Task.Delay(3000);
+            IsMessageVisible = false;
+        }
     }
 }
